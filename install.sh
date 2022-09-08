3320d07f95ea4649d438df2b7e5f7ac6026ee843
Turlx <arussellaaron@gmail.com>
2022-09-07 21:33:26 -0700

    Simple Install File
    Simple Debian Install File

---
+++ install.sh
@@ -0,0 +1,25 @@
+
+#dependencies
+sudo apt-get install nodejs npm
+
+#install dir
+mkdir /etc/ipmifan
+mv ipmifan.js /etc/ipmifan/ipmifan.js
+mv config.json /etc/ipmifan/config.json
+cd /etc/ipmifan && npm i commander
+
+cat <<EOF > /etc/systemd/system/ipmifan.service
+[Unit]
+Description=IPMIFan Javascript Fan Controller
+
+[Service]
+Type=simple
+ExecStart=node /etc/ipmifan/ipmifan.js
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl start ipmifan.service
+systemctl daemon-reload
+systemctl enable ipmifan.service
